9d108e46b5b6f2149f88c19b848cd4918c7da12a
arne roelens <aro@meditech-pharma.com>
2018-08-07 17:11:04 +0200

    version_107

---
+++ src/product/Search.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ARO
+ * Date: 07/08/2018
+ * Time: 09:58
+ */
+
+namespace Datapharma\Product;
+
+class Search
+{
+   public static function version()
+   {
+      return 'DataPharma package v_1.0.5';
+   }
+}
