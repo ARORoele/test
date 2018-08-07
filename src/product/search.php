0b6a4fe329100328f69fb637007be2e6447c5300
arne roelens <aro@meditech-pharma.com>
2018-08-07 13:26:00 +0200

    reinitialized files

---
+++ src/product/search.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ARO
+ * Date: 07/08/2018
+ * Time: 09:58
+ */
+
+namespace product;
+
+class search
+{
+   public static function search()
+   {
+      return 'Give me a product';
+   }
+}
